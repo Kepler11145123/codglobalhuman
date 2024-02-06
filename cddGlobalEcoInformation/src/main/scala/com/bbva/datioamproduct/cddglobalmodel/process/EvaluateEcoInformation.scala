a65b4fedbcce41ec9b549905b01811144c1c9087
Jorge Ayala <jayalaga@emeal.nttdata.com>
2024-02-06 17:49:27 -0600

    Update

--- cddGlobalEcoInformation/src/main/scala/com/bbva/datioamproduct/cddglobalmodel/process/EvaluateEcoInformation.scala
+++ cddGlobalEcoInformation/src/main/scala/com/bbva/datioamproduct/cddglobalmodel/process/EvaluateEcoInformation.scala
@@ -1,27 +1,25 @@
 package com.bbva.datioamproduct.cddglobalmodel.process
+
+import com.datio.dataproc.sdk.datiofilesystem.DatioFileSystem
 import com.bbva.datioamproduct.cddglobalmodel.data.ParametryEcoInformation.{LAUNCHER, WRITE_TEMP, BOOLEAN_TRUE}
 import com.bbva.datioamproduct.cddglobalmodel.data.{GenerateEcoInformation, GetDataProcessEcoInformation, ParametryEcoInformation}
 import com.bbva.datioamproduct.utils.io.WriterWithDataproc
 import com.datio.dataproc.sdk.api.context.RuntimeContext
 import com.datio.dataproc.sdk.datiosparksession.DatioSparkSession
 import com.typesafe.scalalogging.LazyLogging
-import com.typesafe.config.Config
-import org.apache.hadoop.fs.{FileSystem, Path}
-import org.apache.spark.sql.SparkSession
 
 class EvaluateEcoInformation(runtimeContext: RuntimeContext) extends LazyLogging {
-  def run: Int = {
+  def run(): Int = {
     var exitCode = ParametryEcoInformation.EXIT_CODE_INITIAL
-    val config = runtimeContext.getConfig.getConfig(LAUNCHER)
-    val datioSparkSession = DatioSparkSession.getOrCreate()
-    val spark = datioSparkSession.getSparkSession
-    spark.sparkContext.setCheckpointDir(config.getString(WRITE_TEMP))
-
     try {
+      val datioSparkSession = DatioSparkSession.getOrCreate()
+      val spark = datioSparkSession.getSparkSession
+      val config = runtimeContext.getConfig.getConfig(LAUNCHER).resolve()
+
       val inputs = new GetDataProcessEcoInformation(spark, config).getInputs
       val dfEcoInformation = new GenerateEcoInformation(spark, config).generateEcoInformation(inputs)
       new WriterWithDataproc(config).apply(dfEcoInformation, ParametryEcoInformation.OUTPUT_ROUTE)
-      deleteTmpPath(config.getString(WRITE_TEMP),spark)
+      DatioFileSystem.get().dropPartition(config.getString(WRITE_TEMP))
       exitCode = ParametryEcoInformation.EXIT_CODE_SUCCESS
     } catch {
       case permissionException: org.apache.hadoop.security.AccessControlException =>
@@ -32,12 +30,5 @@ class EvaluateEcoInformation(runtimeContext: RuntimeContext) extends LazyLogging
         exitCode = ParametryEcoInformation.EXIT_CODE_FAIL_GENERAL
     }
     exitCode
-  }
-
-  def deleteTmpPath(path: String, spark: SparkSession): Boolean = {
-    val hadoopConfig = spark.sparkContext.hadoopConfiguration
-    val hadoopFileSystem = FileSystem.get(hadoopConfig)
-    val pathHdfsPartition = new Path(path)
-    hadoopFileSystem.delete(pathHdfsPartition, BOOLEAN_TRUE)
   }
 }
