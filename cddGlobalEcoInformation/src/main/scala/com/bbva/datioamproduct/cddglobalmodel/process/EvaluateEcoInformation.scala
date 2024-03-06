4c11ce9f1f5ac391e0dda248c68dc97d44790120
Jorge Eduardo Ayala Garcia <jorgeeduardo.ayala.contractor@bbva.com>
2024-03-06 08:30:52 -0600

    Update evaluate drop compactor functions

--- cddGlobalEcoInformation/src/main/scala/com/bbva/datioamproduct/cddglobalmodel/process/EvaluateEcoInformation.scala
+++ cddGlobalEcoInformation/src/main/scala/com/bbva/datioamproduct/cddglobalmodel/process/EvaluateEcoInformation.scala
@@ -22,7 +22,6 @@ class EvaluateEcoInformation(runtimeContext: RuntimeContext) extends LazyLogging
       val inputs = new GetDataProcessEcoInformation(spark, config).getInputs
       val dfEcoInformation = new GenerateEcoInformation(spark, config).generateEcoInformation(inputs)
       new WriterWithDataproc(config).apply(dfEcoInformation, ParametryEcoInformation.OUTPUT_ROUTE)
-      compactor(config.getString(PATH_OUTPUT), dfEcoInformation, config.getStringList(PARTITIONS).toArray.map(_.toString), spark)
       exitCode = ParametryEcoInformation.EXIT_CODE_SUCCESS
     } catch {
       case permissionException: org.apache.hadoop.security.AccessControlException =>
@@ -36,27 +35,6 @@ class EvaluateEcoInformation(runtimeContext: RuntimeContext) extends LazyLogging
       delete(config.getString(WRITE_TEMP_DELETE))
     }
     exitCode
-  }
-
-  def compactor(tablePath: String, dataFrame: DataFrame, partitionsCols: Array[String], spark: SparkSession): Unit = {
-    val valuesPartitions = dataFrame.select(partitionsCols.map(m => col(m)): _*).dropDuplicates().collect.toList
-    val ListPartitions = valuesPartitions.map { valores =>
-      var ruta = PARAM_EMPTY
-      for {elem <- 1 to partitionsCols.length} {
-        ruta = ruta.concat(SLASH)
-        ruta = ruta.concat(partitionsCols(elem - 1))
-        ruta = ruta.concat(EQUALS)
-        ruta = ruta.concat(valores(elem - 1).toString)
-      }
-      ruta
-    }
-    val builder = CompactorBuilder()
-      .withSparkSession(spark).withFormat(FORMAT).withBlockSize(Some(BLOCK_SIZE_COMPACTOR))
-      .withOverwrite(COMPACTOR_OVERWRITE).withCompactionMode(Some(MODE_COMPACTOR))
-      .withPartitionsFilter(ListPartitions)
-    val compactor = builder.build
-    val report = compactor.compactTable(tablePath, Some(tablePath + SUFFIX_COMPACTOR_PATH))
-    logger.info(report.getSummary)
   }
 
   def delete(path: String): Boolean = {
